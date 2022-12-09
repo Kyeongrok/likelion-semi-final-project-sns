98aa59e92ace279f9ad2adce1ede7e57d19c5d34
Kyeongrok <oceanfog1@gmail.com>
2022-12-09 20:28:05 +0900

    first commit

---
+++ src/main/java/com/likelion/semifinal/LikelionSemiFinalProjectSnsApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.semifinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionSemiFinalProjectSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionSemiFinalProjectSnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/semifinal/LikelionSemiFinalProjectSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.semifinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionSemiFinalProjectSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
